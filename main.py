d653bcc14c6a445ee0b88102e5eedde3ca7d764f
Marton Jakab <jaksy.dev@gmail.com>
2024-11-30 19:48:12 +0100

    Add posting to facebook feed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from opensky_api import OpenSkyApi
 from time import time, gmtime
-from math import floor
 import pandas as pd
+import requests
 
 from dotenv import load_dotenv
 import os
@@ -10,6 +10,7 @@ load_dotenv()
 
 username = os.getenv("USERNAME", None)
 password = os.getenv("PASSWORD", None)
+access_token = os.getenv("ACCESS_TOKEN", "")
 
 # Get airport data
 df = pd.read_csv("world-airports.csv")
@@ -17,16 +18,18 @@ df = pd.read_csv("world-airports.csv")
 api = OpenSkyApi(username,password)
 
 ner_jets= [
-            "440241", #OE-LOT,
+        #   "440241", #OE-LOT,
             "440d18", #OE-HUG
             "440214", #OE-HOZ,
             "440b18", #OE-XWY
             "4401d7" #OE-LEM 
             ]
 
+epoch = int(time())
+interval = 86400 # 24 hours
 
 for icao in ner_jets:
-    data = api.get_flights_by_aircraft(icao,floor(time()) - 2592000, floor(time())-2000)
+    data = api.get_flights_by_aircraft(icao, epoch-interval, epoch)
     if data is not None:
         for flight in data:
             #print the data
@@ -70,4 +73,20 @@ for icao in ner_jets:
             lhour = gmtime(flight.lastSeen).tm_hour
             lmin = gmtime(flight.lastSeen).tm_min
 
-            print(f"Aircraft: {icao}\nCallsign: {callsign}\nFirst seen: {fyear}.{fmon}.{fday} {fhour}:{fmin} GMT\nDeparture: {dep_name}, {dep_country}, {dep_region}, {dep_municipality}\nLast seen: {lyear}.{lmon}.{lday} {lhour}:{lmin} GMT\nArrival: {arr_name}, {arr_country}, {arr_region}, {arr_municipality}\n")
+            # print(f"Aircraft: {icao}\nCallsign: {callsign}\nFirst seen: {fyear}.{fmon}.{fday} {fhour}:{fmin} GMT\nDeparture: {dep_name}, {dep_country}, {dep_region}, {dep_municipality}\nLast seen: {lyear}.{lmon}.{lday} {lhour}:{lmin} GMT\nArrival: {arr_name}, {arr_country}, {arr_region}, {arr_municipality}\n")
+
+            url = "https://graph.facebook.com/v21.0/476593682207063/feed"
+            payload = {
+                "message": f"Aircraft: {icao}\nCallsign: {callsign}\nFirst seen: {fyear}.{fmon}.{fday} {fhour}:{fmin} GMT\nDeparture: {dep_name}, {dep_country}, {dep_region}, {dep_municipality}\nLast seen: {lyear}.{lmon}.{lday} {lhour}:{lmin} GMT\nArrival: {arr_name}, {arr_country}, {arr_region}, {arr_municipality}\n",
+                "access_token": access_token
+            }
+            headers = {
+                "Content-Type": "application/json"
+            }
+
+            # Send the POST request
+            response = requests.post(url, json=payload, headers=headers)
+
+            # Check the response
+            print("Status Code:", response.status_code)
+            print("Response Body:", response.json())
